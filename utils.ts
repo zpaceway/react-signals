08ad01b849056788d16109da454e7a275df355ab
Alexandro Tapia <zpaceway@gmail.com>
2023-03-03 22:50:29 -0500

    use pseudo uuid to avoid problems with react native

--- utils.ts
+++ utils.ts
@@ -1,9 +1,21 @@
 export const uuid4 = () => {
-  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (seed) => {
-    const number = parseInt(seed);
-    return (
-      number ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (number / 4)))
-    ).toString(16);
+  let d = new Date().getTime();
+  let d2 =
+    (typeof performance !== "undefined" &&
+      performance.now &&
+      performance.now() * 1000) ||
+    0; //Time in microseconds since page-load or 0 if unsupported
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    let r = Math.random() * 16; //random number between 0 and 16
+    if (d > 0) {
+      //Use timestamp until depleted
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      //Use microseconds since page-load if supported
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
   });
 };
